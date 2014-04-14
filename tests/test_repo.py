24d2e9a62515299db05dff5f77d90b0b66cf0e87
Florian Wilhelm <Florian.Wilhelm@blue-yonder.com>
2014-04-14 16:10:50 +0200

    pep8 fixes

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -14,8 +14,7 @@ __license__ = "new BSD"
 def test_init_commit_repo(tmpdir):
     project = "my_project"
     struct = {project: {
-                  "my_file": "Some other content"
-              }}
+        "my_file": "Some other content"}}
     structure.create_structure(struct)
     repo.init_commit_repo(project, struct)
     assert os.path.exists(os.path.join(project, ".git"))
