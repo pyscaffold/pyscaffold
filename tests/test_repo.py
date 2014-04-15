bc715de0cc48c91ff0f9d7266d61334271c04bb2
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2014-04-15 19:43:42 +0200

    More code coverage for repo.py

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -5,6 +5,8 @@ import os.path
 from pyscaffold import repo
 from pyscaffold import structure
 
+import pytest
+
 from .fixtures import tmpdir
 
 __author__ = "Florian Wilhelm"
@@ -15,7 +17,18 @@ __license__ = "new BSD"
 def test_init_commit_repo(tmpdir):
     project = "my_project"
     struct = {project: {
-        "my_file": "Some other content"}}
+        "my_file": "Some other content",
+        "my_dir": {"my_file": "Some more content"}}
+    }
     structure.create_structure(struct)
     repo.init_commit_repo(project, struct)
     assert os.path.exists(os.path.join(project, ".git"))
+
+
+def test_init_commit_repo_with_wrong_structure(tmpdir):
+    project = "my_project"
+    struct = {project: {
+        "my_file": type("StrangeType", (object,), {})}}
+    os.mkdir(project)
+    with pytest.raises(RuntimeError):
+        repo.init_commit_repo(project, struct)
