fb075ad4bffe1c7274cc3596731f667590468db0
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2014-04-18 15:10:35 +0200

    Changed order in system test solve Travis problem

--- tests/system_test.sh
+++ tests/system_test.sh
@@ -4,17 +4,17 @@ set -e -x
 # Install additional requirements
 pip install pytest sphinx
 
-# Setup a test project and run some common tasks
+# Setup a test project
 PROJECT="my_project"
 putup $PROJECT
+# Try updating
+DESCRIPTION="new_description"
+putup --update $PROJECT -d $DESCRIPTION
+test `python $PROJECT/setup.py --description` == $DESCRIPTION
+# Run some common tasks
 cd $PROJECT
 python setup.py test
 python setup.py docs
 python setup.py version
 python setup.py sdist
 python setup.py bdist
-cd ..
-DESCRIPTION="new_description"
-putup --update $PROJECT -d $DESCRIPTION
-cd $PROJECT
-test `python setup.py --description` == $DESCRIPTION
