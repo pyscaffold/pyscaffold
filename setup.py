3d83fbd2b96c55ca16cb907f54828f4f1cb30377
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2014-05-02 14:49:20 +0200

    Updated PyScaffold with itself

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """
-    Setup file of PyScaffold
+    Setup file for PyScaffold.
+
+    This file was generated with PyScaffold 0.4.1.post7, a tool that easily
+    puts up a scaffold for your new Python project. Learn more under:
+    https://github.com/blue-yonder/pyscaffold
 """
 
 import os
@@ -12,12 +15,13 @@ from distutils.cmd import Command
 
 import versioneer
 import setuptools
-from setuptools import setup
 from setuptools.command.test import test as TestCommand
+from setuptools import setup
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(
     inspect.getfile(inspect.currentframe())))
 
+# Change these settings according to your needs
 MAIN_PACKAGE = "pyscaffold"
 DESCRIPTION = "Tool for easily putting up the scaffold of a Python project"
 LICENSE = "new BSD"
@@ -25,9 +29,13 @@ URL = "https://github.com/blue-yonder/pyscaffold"
 AUTHOR = "Florian Wilhelm"
 EMAIL = "Florian.Wilhelm@blue-yonder.com"
 
+COVERAGE_XML = False
+COVERAGE_HTML = False
+JUNIT_XML = False
+
 # Add here all kinds of additional classifiers as defined under
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-classifiers = ['Development Status :: 4 - Beta',
+CLASSIFIERS = ['Development Status :: 4 - Beta',
                'Programming Language :: Python',
                'Programming Language :: Python :: 2.6',
                'Programming Language :: Python :: 2.7',
@@ -39,7 +47,8 @@ classifiers = ['Development Status :: 4 - Beta',
                'Operating System :: POSIX :: Linux',
                'Topic :: Utilities']
 
-console_scripts = ['putup = pyscaffold.runner:run']
+# Add here console scripts like ['hello_world = pyscaffold.module:function']
+CONSOLE_SCRIPTS = ['putup = pyscaffold.runner:run']
 
 # Versioneer configuration
 versioneer.versionfile_source = os.path.join(MAIN_PACKAGE, '_version.py')
@@ -49,16 +58,28 @@ versioneer.parentdir_prefix = MAIN_PACKAGE + '-'
 
 
 class PyTest(TestCommand):
-    user_options = [("cov=", None, "Run coverage")]
+    user_options = [("cov=", None, "Run coverage"),
+                    ("cov-xml=", None, "Generate junit xml report"),
+                    ("cov-html=", None, "Generate junit html report"),
+                    ("junitxml=", None, "Generate xml of test results")]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.cov = None
+        self.cov_xml = False
+        self.cov_html = False
+        self.junitxml = None
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
         if self.cov is not None:
             self.cov = ["--cov", self.cov, "--cov-report", "term-missing"]
+            if self.cov_xml:
+                self.cov.extend(["--cov-report", "xml"])
+            if self.cov_html:
+                self.cov.extend(["--cov-report", "html"])
+        if self.junitxml is not None:
+            self.junitxml = ["--junitxml", self.junitxml]
 
     def run_tests(self):
         try:
@@ -70,6 +91,8 @@ class PyTest(TestCommand):
         if self.cov:
             params["args"] += self.cov
             params["plugins"] = ["cov"]
+        if self.junitxml:
+            params["args"] += self.junitxml
         errno = pytest.main(**params)
         sys.exit(errno)
 
@@ -104,7 +127,7 @@ def sphinx_builder():
 
 def get_install_requirements(path):
     content = open(os.path.join(__location__, path)).read()
-    return [req for req in content.split("\n") if req != '']
+    return [req for req in content.split("\\n") if req != '']
 
 
 def read(fname):
@@ -125,6 +148,30 @@ install_reqs = get_install_requirements("requirements.txt")
 
 
 def setup_package():
+    command_options = {
+        'docs': {'project': ('setup.py', MAIN_PACKAGE),
+                 'version': ('setup.py', version.split('-', 1)[0]),
+                 'release': ('setup.py', version),
+                 'build_dir': ('setup.py', docs_build_path),
+                 'config_dir': ('setup.py', docs_path),
+                 'source_dir': ('setup.py', docs_path)},
+        'doctest': {'project': ('setup.py', MAIN_PACKAGE),
+                    'version': ('setup.py', version.split('-', 1)[0]),
+                    'release': ('setup.py', version),
+                    'build_dir': ('setup.py', docs_build_path),
+                    'config_dir': ('setup.py', docs_path),
+                    'source_dir': ('setup.py', docs_path),
+                    'builder': ('setup.py', 'doctest')},
+        'test': {'test_suite': ('setup.py', 'tests'),
+                 'cov': ('setup.py', 'pyscaffold')}
+                       }
+    if JUNIT_XML:
+        command_options['test']['junitxml'] = ('setup.py', 'junit.xml')
+    if COVERAGE_XML:
+        command_options['test']['cov_xml'] = ('setup.py', True)
+    if COVERAGE_HTML:
+        command_options['test']['cov_html'] = ('setup.py', True)
+
     setup(name=MAIN_PACKAGE,
           version=version,
           url=URL,
@@ -133,32 +180,14 @@ def setup_package():
           author_email=EMAIL,
           license=LICENSE,
           long_description=read('README.rst'),
-          classifiers=classifiers,
+          classifiers=CLASSIFIERS,
           test_suite='tests',
           packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
           install_requires=install_reqs,
           cmdclass=cmdclass,
           tests_require=['pytest-cov', 'pytest'],
-          command_options={
-              'docs': {'project': ('setup.py', MAIN_PACKAGE),
-                       'version': ('setup.py', version.split('-', 1)[0]),
-                       'release': ('setup.py', version),
-                       'build_dir': ('setup.py', docs_build_path),
-                       'config_dir': ('setup.py', docs_path),
-                       'source_dir': ('setup.py', docs_path)},
-              'doctest': {'project': ('setup.py', MAIN_PACKAGE),
-                          'version': ('setup.py', version.split('-', 1)[0]),
-                          'release': ('setup.py', version),
-                          'build_dir': ('setup.py', docs_build_path),
-                          'config_dir': ('setup.py', docs_path),
-                          'source_dir': ('setup.py', docs_path),
-                          'builder': ('setup.py', 'doctest')},
-              'test': {'test_suite': ('setup.py', 'tests'),
-                       'cov': ('setup.py', 'pyscaffold')}
-          },
-          include_package_data=True,
-          package_data={MAIN_PACKAGE: ['data/*']},
-          entry_points={'console_scripts': console_scripts})
+          command_options=command_options,
+          entry_points={'console_scripts': CONSOLE_SCRIPTS})
 
 if __name__ == "__main__":
     setup_package()
