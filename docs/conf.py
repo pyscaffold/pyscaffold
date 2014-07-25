f2706da610f2e12df3877b70695ca0610d839f3f
Felix Wick <felix.wick@blue-yonder.com>
2014-07-25 10:48:21 +0200

    show version in html title of documentation
    this requires pyscaffold to be installed before building the docs with make

--- docs/conf.py
+++ docs/conf.py
@@ -121,7 +121,9 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+from pyscaffold import __name__ as proj_name
+from pyscaffold import __version__ as proj_version
+html_title = ' '.join([proj_name, ''.join(['v', proj_version]), 'documentation'])
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
