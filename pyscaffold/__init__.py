3f9bdfd91429bcec643bde01bb6c6a11c51c59cb
Florian Wilhelm <Florian.Wilhelm@blue-yonder.com>
2015-04-28 09:15:14 +0200

    Fixed double entries in _version.py

--- pyscaffold/__init__.py
+++ pyscaffold/__init__.py
@@ -2,7 +2,3 @@ from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
