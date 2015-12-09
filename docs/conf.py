61fbebf80233438ba17ae00bfea3c258ee054fdb
Florian Wilhelm <florian.wilhelm@gmail.com>
2015-12-09 22:41:18 +0100

    Logo for pdf

--- docs/conf.py
+++ docs/conf.py
@@ -217,7 +217,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-# latex_logo = ""
+latex_logo = "gfx/logo.png"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
