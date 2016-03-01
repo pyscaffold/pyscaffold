9f82fbdca76720a2f3ef3400e4432c1b4b24fb6e
Florian Wilhelm <florian.wilhelm@gmail.com>
2016-03-01 21:33:21 +0100

    Unittests fixed

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -32,16 +32,16 @@ def test_version2str():
 def test_local_version2str():
     ver = ScmVersion('1.0', dirty=True, node='abcdef')
     ver_str = integration.local_version2str(ver)
-    assert ver_str == '+abcdef.dirty'
+    assert ver_str == '+nabcdef.dirty'
     ver = ScmVersion('1.0', dirty=False)
     ver_str = integration.local_version2str(ver)
     assert ver_str == ''
     ver = ScmVersion('1.0', distance=1, dirty=True, node='abcdef')
     ver_str = integration.local_version2str(ver)
-    assert ver_str == '+abcdef.dirty'
+    assert ver_str == '+nabcdef.dirty'
     ver = ScmVersion('1.0', distance=1, dirty=False, node='abcdef')
     ver_str = integration.local_version2str(ver)
-    assert ver_str == '+abcdef'
+    assert ver_str == '+nabcdef'
 
 
 def test_build_cmd_docs():
