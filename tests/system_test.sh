450d44d8f8a1b693c99d949a7888fe193238710a
Florian Wilhelm <florian.wilhelm@gmail.com>
2016-04-22 20:53:13 +0200

    Trying to fix the libgfortran travis problem again...

--- tests/system_test.sh
+++ tests/system_test.sh
@@ -60,7 +60,7 @@ if [[ "${DISTRIB}" == "conda" && "${PYTHON_VERSION}" == "2.7" ]]; then
     git clone --branch v0.2.1 https://github.com/blue-yonder/pydse.git pydse
     cp ${TRAVIS_BUILD_DIR}/tests/misc/pydse_setup.cfg pydse/setup.cfg
     putup --update pydse
-    conda install --yes nomkl numpy=1.10.4 scipy=0.16 matplotlib libgfortran
+    conda install --yes nomkl numpy scipy matplotlib libgfortran
     pip install -v -r pydse/requirements.txt
     run_common_tasks pydse
     cd ..
