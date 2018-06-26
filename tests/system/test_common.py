4c029a0e9a9023f4cd7e3b7052e5218df70d103f
Anderson Bravalheri <andersonbravalheri@gmail.com>
2018-06-26 14:12:35 +0000

    Add force overwrite and extension tests

--- tests/system/test_common.py
+++ tests/system/test_common.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from os import environ
 from os.path import isdir
 
 import pytest
@@ -17,7 +18,7 @@ def run_common_tasks(venv, tests=True, flake8=True):
     venv.run('python setup.py sdist')
     venv.run('python setup.py bdist')
 
-    if flake8:
+    if flake8 and environ.get('coverage') == 'true':
         venv.install('flake8')
         venv.run('flake8 --count')
 
@@ -34,8 +35,8 @@ def test_putup(venv, tmpdir):
 
 def test_putup_with_update(venv, tmpdir):
     # Given a venv with pyscaffold installed,
+    # and a project already created
     with tmpdir.as_cwd():
-        # and a project was already created
         venv.run('putup myproj')
         # when we run putup with the update flag
         venv.run('putup --update myproj')
@@ -56,3 +57,34 @@ def test_differing_package_name(venv, tmpdir):
     # then no error should be raised when running the common tasks
     with tmpdir.join('my-cool-proj').as_cwd():
         run_common_tasks(venv)
+
+
+def test_force_overwrite(venv, tmpdir):
+    # Given a venv with pyscaffold installed,
+    # and a project already created
+    with tmpdir.as_cwd():
+        venv.run('putup myproj')
+        # when it is forcefully updated
+        venv.run('putup --force --tox myproj')
+    # then things should still work
+    if environ.get('DISTRIB') == 'ubuntu':
+        with tmpdir.join('myproj').as_cwd():
+            venv.run('tox -e py')
+
+
+@pytest.mark.parametrize('extension, kwargs', (
+    ('pre-commit', {}),
+    ('travis', {}),
+    ('gitlab', {}),
+    ('django', {'flake8': False}),
+    ('no-skeleton', {'tests': False})
+))
+def test_extensions(venv, tmpdir, extension, kwargs):
+    # Given pyscaffold is installed,
+    with tmpdir.as_cwd():
+        # when we call putup with extensions
+        name = 'myproj' + extension
+        venv.run('putup', '--'+extension, name)
+        with tmpdir.join(name).as_cwd():
+            # then things still should work as expected
+            run_common_tasks(venv, **kwargs)
