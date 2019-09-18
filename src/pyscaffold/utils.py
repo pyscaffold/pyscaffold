04aa52dabffc8cfc82ae1a8fa8f53acb65bd619f
Florian Wilhelm <Florian.Wilhelm@inovex.de>
2019-09-18 15:01:12 +0200

    Another test

--- src/pyscaffold/utils.py
+++ src/pyscaffold/utils.py
@@ -234,9 +234,10 @@ def create_file(path, content, pretend=False):
         pretend (bool): false by default. File is not written when pretending,
             but operation is logged.
     """
+    content = content.replace(os.linesep, '\n')
     if not pretend:
-        with open(path, 'wb') as fh:
-            fh.write(content.encode('utf-8'))
+        with open(path, 'w', encoding='utf-8') as fh:
+            fh.write(content)
 
     logger.report('create', path)
 
