16716d9d81f88bbc60ff8fcff54f654651b1fa6e
Anderson Bravalheri <andersonbravalheri@gmail.com>
2020-07-21 14:24:02 +0100

    Improve docs on file op modifiers

--- src/pyscaffold/operations.py
+++ src/pyscaffold/operations.py
@@ -1,7 +1,37 @@
 """
 Collection of functions responsible for the "reification" of the file representation in
-the project structure into a file written to the disk. A function that "reifies" a file
-is called here a **file operation** or simply a **file op**.
+the project structure into a file written to the disk.
+
+A function that "reifies" a file (manifests the programmatic representation of the file
+in the project structure dictionary to the disk) is called here a **file operation** or
+simply a **file op**. Actually file ops don't even need to be functions strictly
+speaking, they can be any :obj:`callable` object. The only restriction is that file ops
+**MUST** respect the :obj:`FileOp` signature. The :obj:`create` function is a good
+example of how to write a new file op.
+
+A function (or callable) that modifies the behaviour of an existing file op, by wrapping
+it with another function/callable is called here **modifier**. Modifiers work similarly
+to Python `decorators`_ and allow extending/refining the existing file ops.
+A modifier should receive a file op as argument and return another file op.
+:obj:`no_overwrite` and :obj:`skip_on_update` are good examples on how to write new file
+op modifiers.
+
+While modifiers don't have a specific signature (the number of parameters might vary,
+but they always return a single file op value), the following conventions **SHOULD** be
+observed when creating new modifiers:
+
+- Modifiers should accept at least one argument: the **file op** they modify
+  (you don't have to be very creative, go ahead and name this parameter ``file_op``, it
+  is a good convention).
+  This parameter should be made **optional** (the default value of :obj:`create` usually
+  works, unless there is a better default value for the main use case of the modifier).
+- Modifiers can accept arguments other then the file op they modify. These arguments
+  should **precede** the file op in the list of arguments (the file op should be the
+  last argument, which interoperates well with :obj:`~functools.partial`).
+- When writing a modifier that happens to be a function (instead of a callable class),
+  please name the inner function with the same name of the modifier but preceded by an
+  ``_`` (underscore) char. This allows better inspection/debugging.
+
 
 .. versionchanged:: 4.0
    Previously, file operations where simply indicated as a numeric flag
@@ -24,9 +54,9 @@ from .log import logger
 
 ScaffoldOpts = Dict[str, Any]
 """Dictionary with PyScaffold's options, see :obj:`pyscaffold.api.create_project`.
-Should be treated as immutable, if required clone before changing.
+Should be treated as immutable (if required, copy before changing).
 
-Please notice some behaviours given by the options should be observed. For example,
+Please notice some behaviours given by the options **SHOULD** be observed. For example,
 files should be overwritten when the **force** option is ``True``. Similarly when
 **pretend** is ``True``, no operation should be really performed, but any action should
 be logged as if realized.
@@ -55,11 +85,12 @@ Returns:
     might return :obj:`None` if a pre-existing file in the disk is not modified.
 
 Note:
-    A **FileOp** usually has side effects (e.g. write a file to the disk).
+    A **FileOp** usually has side effects (e.g. write a file to the disk), see
+    :obj:`FileFileContents` and :obj:`ScaffoldOpts` for other conventions.
 """
 
 
-# FileOps and FileOp "factories/decorators/wrappers"
+# FileOps and FileOp modifiers (a.k.a. factories/decorators/wrappers)
 
 
 def create(path: Path, contents: FileContents, opts: ScaffoldOpts) -> Union[Path, None]:
@@ -72,9 +103,8 @@ def create(path: Path, contents: FileContents, opts: ScaffoldOpts) -> Union[Path
 
 
 def no_overwrite(file_op: FileOp = create) -> FileOp:
-    """This function is not exactly a :obj:`FileOp`, instead, when called it returns a
-    :obj:`FileOp` that do not overwrite an existing file during update (still created if
-    not exists). It works similarly to a `decorator`_.
+    """File op modifier. Returns a :obj:`FileOp` that do not overwrite an existing file
+    during update (still created if not exists).
 
     Args:
         file_op: a :obj:`FileOp` that will be "decorated",
@@ -94,9 +124,8 @@ def no_overwrite(file_op: FileOp = create) -> FileOp:
 
 
 def skip_on_update(file_op: FileOp = create) -> FileOp:
-    """This function is not exactly a :obj:`FileOp`, instead, when called it returns a
-    :obj:`FileOp` that do not create the file during an update.
-    It works similarly to a `decorator`_.
+    """File op modifier. Returns a :obj:`FileOp` that do will skip the file during a
+    project update (the file will just be created for brand new projects).
 
     Args:
         file_op: a :obj:`FileOp` that will be "decorated",
@@ -116,9 +145,8 @@ def skip_on_update(file_op: FileOp = create) -> FileOp:
 
 
 def add_permissions(permissions: int, file_op: FileOp = create) -> FileOp:
-    """This function is not exactly a :obj:`FileOp`, instead, when called it returns a
-    :obj:`FileOp` that will **add** permissions to the file (on top of the ones given by
-    default by the OS). It works similarly to a `decorator`_.
+    """File op modifier. Returns a :obj:`FileOp` that will **add** access permissions to
+    the file (on top of the ones given by default by the OS).
 
     Args:
         permissions (int): permissions to be added to file::
@@ -128,9 +156,9 @@ def add_permissions(permissions: int, file_op: FileOp = create) -> FileOp:
             Preferably the values should be a combination of
             :obj:`stat.S_* <stat.S_IRUSR>` values (see :obj:`os.chmod`).
 
-        file_op: a :obj:`FileOp` that will be "decorated",
-            i.e. if the file exists in disk after ``file_op`` is called (either created
-            or pre-existing) it will add ``permissions`` to it.
+        file_op: a :obj:`FileOp` that will be "decorated".
+            If the file exists in disk after ``file_op`` is called (either created
+            or pre-existing), ``permissions`` will be added to it.
             Default: :obj:`create`.
 
     Note:
