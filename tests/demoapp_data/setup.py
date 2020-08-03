b66ceb8ff823275b4c2e346dd892a9f696a7b36e
Anderson Bravalheri <andersonbravalheri@gmail.com>
2020-08-03 01:21:31 +0100

    Update demoapps to not require PyScaffold for build

--- tests/demoapp_data/setup.py
+++ tests/demoapp_data/setup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
-"""
-    Setup file for our unittest demo application
-"""
+"""Setup file for our unittest demo application"""
 
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup(use_scm_version={"version_scheme": "post-release"})
