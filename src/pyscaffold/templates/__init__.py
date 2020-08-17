3f4fe36a71822690bb7267071b9c94114266acb6
Anderson Bravalheri <andersonbravalheri@gmail.com>
2020-08-17 17:28:39 +0100

    Add TODO note for importlib in templates/__init__.py

--- src/pyscaffold/templates/__init__.py
+++ src/pyscaffold/templates/__init__.py
@@ -15,6 +15,7 @@ from .. import dependencies as deps
 from .. import toml
 
 if sys.version_info[:2] >= (3, 7):
+    # TODO: Import directly (no need for workaround) when `python_requires = >= 3.7`
     from importlib.resources import read_text
 else:
     from pkgutil import get_data
