cec45b06fad0a851522e354f468bf47e9cbf9ca3
Anderson Bravalheri <andersonbravalheri@gmail.com>
2020-10-30 18:02:15 +0000

    Add some tests for ErrorLoadingException error message

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,10 +1,17 @@
 import logging
 import re
+import sys
 
 import pytest
 
-from pyscaffold.exceptions import exceptions2exit
+from pyscaffold.exceptions import ErrorLoadingException, exceptions2exit
 from pyscaffold.log import logger
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import EntryPoint  # pragma: no cover
+else:
+    from importlib_metadata import EntryPoint  # pragma: no cover
 
 
 def test_exceptions2exit():
@@ -27,3 +34,17 @@ def test_exceptions2exit_verbose(capsys):
     error = capsys.readouterr().err
     match = re.search(r"raise RuntimeError", error)
     assert match
+
+
+def test_error_loading_external_extension():
+    # Assert the error message displays the correct python package name
+    extension = "pyscaffoldext.fake.extension"
+
+    # Extension name is given directly
+    ex = ErrorLoadingException(extension)
+    assert "an error loading 'pyscaffoldext-fake'" in str(ex)
+
+    # Entrypoint is given
+    fake = EntryPoint("fake", f"{extension}:Fake", "pyscaffold.cli")
+    ex = ErrorLoadingException(entry_point=fake)
+    assert "an error loading 'pyscaffoldext-fake'" in str(ex)
