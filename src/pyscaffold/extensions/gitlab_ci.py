2eda0e5a85e3f71e8250ebc517fe9fc4f0adb95a
Anderson Bravalheri <andersonbravalheri@gmail.com>
2022-03-08 19:13:50 +0000

    Make sure --gitlab extension automatically activate pre-commit

--- src/pyscaffold/extensions/gitlab_ci.py
+++ src/pyscaffold/extensions/gitlab_ci.py
@@ -1,18 +1,28 @@
 """
 Extension that generates configuration and script files for GitLab CI.
 """
-
+from argparse import ArgumentParser
 from typing import List
 
 from .. import structure
 from ..actions import Action, ActionParams, ScaffoldOpts, Structure
 from ..operations import no_overwrite
 from ..templates import get_template
-from . import Extension
+from . import Extension, include
+from .pre_commit import PreCommit
 
 
 class GitLab(Extension):
     """Generate GitLab CI configuration files"""
+
+    def augment_cli(self, parser: ArgumentParser):
+        """Augments the command-line interface parser.
+        See :obj:`~pyscaffold.extension.Extension.augment_cli`.
+        """
+        parser.add_argument(
+            self.flag, help=self.help_text, nargs=0, action=include(PreCommit(), self)
+        )
+        return self
 
     def activate(self, actions: List[Action]) -> List[Action]:
         """Activate extension, see :obj:`~pyscaffold.extension.Extension.activate`."""
